02606acf95ec980b0830938d7941c32770b21772
daliladiyanasaladin <115535455+daliladiyanasaladin@users.noreply.github.com>
2022-12-30 10:37:14 +0800

    Update PokeDex.js

--- src/PokeDex.js
+++ src/PokeDex.js
@@ -3,11 +3,25 @@ import { useState, useEffect } from "react";
 import ReactLoading from "react-loading";
 import axios from "axios";
 import Modal from "react-modal";
+import { Link, useParams } from "react-router-dom";
 
 function PokeDex() {
   const [pokemons, setPokemons] = useState([]);
   const [pokemonDetail, setPokemonDetail] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
+  const [search, setSearch] = useState('');
+  
+  const [currentPage, setCurrent] = useState(`https://pokeapi.co/api/v2/pokemon`);
+  
+  const [prevPage, setPrev] = useState();
+  
+ //in array bcs to display value in sequence
+  const [sorts, setSorts] = useState([]);
+  // eslint-disable-next-line 
+  const [PokeDetail, setPokeDetail]= useState([]);
+  const [nextPage, setNext] = useState();
+  // eslint-disable-next-line 
+  const {name} = useParams();
 
   const customStyles = {
     content: {
@@ -23,11 +37,130 @@ function PokeDex() {
     overlay: { backgroundColor: "grey" },
   };
 
+//fetch api
+//sort page
+
+  useEffect (() => {
+    axios.get(currentPage)
+    .then (response => {
+      if (sorts === 'name'){
+        const sorting = response.data.results.sort((x,y) =>
+        x.name.localeCompare(y.name));
+        setPokemons(sorting);
+        setIsLoading(false);
+      }
+      else
+      {
+        setIsLoading(false);
+        setPokemons(response.data.results);
+        setPrev(response.data.previous);
+        setNext(response.data.next);
+        
+      }
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  }, [currentPage, sorts])
+  
+ /*
+ const sorting = (e) =>{
+  if(e.target.value === "ace"){
+    setPokemons([...pokemons].sort((a,b)=> a.name > b.name ? 1 : -1))
+  }
+  else if (e.target.value ==== "dsd") {
+    setPokemons([...pokemons].sort((a,b)=> b.name > a.name ? 1 : -1))
+  }
+
+  }
+ }*/
+  const searched = (event)=> {
+    let {value} = event.target;
+    setSearch(value);
+  }
+  function redirectPrev(){
+    setCurrent(prevPage);
+  }
+
+  function redirectNext(){
+    setCurrent(nextPage);
+  }
+  //passing value get from API
+  const pokedexDetail = (name) => {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${name}`)
+    .then(response => {
+      setIsLoading(false);
+      setPokemonDetail(response.data);
+    })
+    .catch(error => {
+      console.log(error);
+    })
+ 
+  }
+  
+
   if (!isLoading && pokemons.length === 0) {
     return (
-      <div>
+      <div>    
         <header className="App-header">
+          <b><ReactLoading/></b>
+
+          <div className="container-welcome">
           <h1>Welcome to pokedex !</h1>
+
+          <div className="search-input">
+            <input
+            className="search-here"
+            type="text"
+            value={search}
+            placeholder="Search Pokemon"
+            onChange={searched}
+            />
+            <select
+            value={pokemons.name}
+            onChange={(e) =>
+            setSorts(e.target.value)}
+            >
+              <option value="default">default</option>
+              <option value="name"> By Name </option>
+
+            </select>
+          </div>
+
+          {pokemons.length > 0 && <div>
+            {
+              pokemons.filter(item => 
+                item.name.toLowerCase().includes(search.toLocaleLowerCase()))
+                .map((item, index)=> {
+                  return <div key={index} className="container-detail" 
+                  onClick={() =>{setPokemonDetail(item); pokedexDetail(item.name)}}>
+                    <Link to={`/pokedex/${item.name}`}>
+                      <img src="item.sprites" alt='' width="50%" height="40%"/>
+                      <h4>{item.name}</h4>
+                    </Link>
+
+                  </div>
+                })
+            }</div>
+            }
+            <div className="page-button">
+              <button
+              type="btn"
+              onClick={redirectPrev}
+              >
+                Previous
+              </button>
+              <button
+              type="btn"
+              onClick={redirectNext}
+              >
+                Next
+              </button>
+            </div>
+          
+
+          </div>
+          
           <h2>Requirement:</h2>
           <ul>
             <li>
@@ -64,6 +197,7 @@ function PokeDex() {
           <>
             <h1>Welcome to pokedex !</h1>
             <b>Implement Pokedex list here</b>
+            <b><ReactLoading /></b>
           </>
         )}
       </header>
@@ -88,6 +222,31 @@ function PokeDex() {
               <li>Create a  buttton to download the information generated in this modal as pdf. (images and chart must be included)</li>
             </ul>
           </div>
+
+          
+          <div className="container-pokemon">
+            
+            <div>
+              <img src={PokeDetail.sprites.front_default} alt='' width="300" height="300"/>
+            </div>
+          
+          <div>
+          <h3>{PokeDetail.species.name}</h3>
+          </div>
+          </div>
+          {
+            PokeDetail.stats.map((item) => {
+              return <div>
+                <table style={{width:'100%'}}>
+                  <tr>
+                    <td style={{width:'40%'}}>{item.base_stat}</td>
+                    <td style={{width:'40%'}}>{item.effort}</td>
+                    <td style={{width:'40%'}}>{item.stat.name}</td>
+                  </tr>
+                </table>
+              </div>
+            })
+          }
         </Modal>
       )}
     </div>
