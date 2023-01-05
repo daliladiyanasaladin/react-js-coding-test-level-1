1854227121f270c14b4a493646bc7d48bea7b945
daliladiyanasaladin <115535455+daliladiyanasaladin@users.noreply.github.com>
2023-01-05 16:15:34 +0800

    WIP

--- src/Page.js
+++ src/Page.js
@@ -1,75 +1,72 @@
-import React, {useState} from "react";
-import "./index.css"
+import React, { useState } from 'react';
+import "./index.css";
 
-const Page = ({data, query, LimitedPage, LimitedData, showModel}) => {
-    const [pages] = useState(Math.round(data.length/LimitedData));
-    const [currentPage, setCurrent] = useState(1);
+const Pagination = ({ data, query, pageLimit, dataLimit, showModel }) => {
+    const [pages] = useState(Math.round(data.length / dataLimit));
+    const [currentPage, setCurrentPage] = useState(1);
 
-    function nextPage() {
-        setCurrent((page) => page + 1);
+    function goToNextPage() {
+        setCurrentPage((page) => page + 1);
     }
 
-    function prevPage() {
-        setCurrent((page) => page - 1);
+    function goToPreviousPage() {
+        setCurrentPage((page) => page - 1);
     }
 
     function changePage(event) {
-        const numberpage = Number(event.target.textContent);
-        setCurrent(numberpage);
+        const pageNumber = Number(event.target.textContent);
+        setCurrentPage(pageNumber);
     }
 
-    const getPageData = () => {
-        const startIndex = currentPage * LimitedData - LimitedPage;
-        const endIndex = startIndex + LimitedData;
+    const getPaginatedData = () => {
+        const startIndex = currentPage * dataLimit - dataLimit;
+        const endIndex = startIndex + dataLimit;
         return data.slice(startIndex, endIndex);
-    }
+    };
 
-    const pageGroup = () => {
-        let start = Math.floor((currentPage-1)/ LimitedPage) * LimitedPage;
-        return new Array(LimitedPage).fill().map((_, idx) => start + idx + 1);
-    }
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPage - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill().map((_, idx) => start + idx + 1);
+    };
 
-    return(
-        <div className="container">
-            {getPageData().filter(poke => {
-                if (query === ''){
+    return (
+        <div className='container'>
+            {getPaginatedData().filter(poke => {
+                if (query === '') {
                     return poke;
-
-                } else if (poke.name.toLowerCase().includes(query.toLowerCase())){
+                } else if (poke.name.toLowerCase().includes(query.toLowerCase())) {
                     return poke;
                 }
-            }).map(({name, url}, index) => 
+            }).map(({ name, url }, index) =>
                 <b className="pokemon" key={index} onClick={() => showModel(url)}>{name}</b>
-                )}
-
-                <div className="page-create">
-                    <button
-                    onClick={prevPage}
+            )}
+            <div className="page">
+                <button id='btn'
+                    onClick={goToPreviousPage}
                     className={`prev ${currentPage === 1 ? 'disabled' : ''}`}
-                    >Previous</button>
+                >
+                    prev
+                </button>
 
-                    {pageGroup().map((item, index)=> 
-                    <button
-                    key={index}
-                    onClick={changePage}
-                    className={`paginationItem ${currentPage === item ? 'active' : null}`}
+                {getPaginationGroup().map((item, index) => (
+                    <button id='btn'
+                        key={index}
+                        onClick={changePage}
+                        className={`paginationItem ${currentPage === item ? 'active' : null}`}
                     >
                         {item}
-                        </button>
-                    )}
-                    <button
-                    onClick={nextPage}
-                    className={`next ${currentPage === pages ? 'disabled' : ''}`}
-                    > Next
-
                     </button>
+                ))}
 
-                </div>
-
+                <button
+                    onClick={goToNextPage}
+                    className={`next ${currentPage === pages ? 'disabled' : ''}`}
+                >
+                    next
+                </button>
+            </div>
         </div>
     )
-
-    
-
 }
-export default Page;
+
+export default Pagination
